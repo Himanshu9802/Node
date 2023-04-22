a5fce1339080dc172f65f4ae5ddfa5f468bc1fc3
Himanshu Nanikwal <himanhunanikwal@gmail.com>
2023-04-23 01:28:37 +0530

    template engines

--- Express/src/index.js
+++ Express/src/index.js
@@ -1,11 +1,29 @@
 const path = require('path')
 const express = require('express')
 const app = express()
+const hbs = require('hbs')
 
 const staticPath = (path.join(__dirname, '../public'))
+const screensPath = (path.join(__dirname, './screens/views'))
+const partialsPath = (path.join(__dirname, './screens/partials'))
 
 // buil-in middleware
-app.use(express.static(staticPath))
+// app.use(express.static(staticPath))
+
+// set the view engine
+app.set('view engine', "hbs")
+// changing views name
+app.set('views',screensPath)
+hbs.registerPartials(partialsPath)
+
+// template engine route
+app.get("",(req, res)=>{
+    res.render('index', {name: "Himanshu"})
+})
+
+app.get('/abt',(req,res)=>{
+    res.render('about')
+})
 
 app.get('/',(req,res)=>{
     res.send('Express Home')
@@ -17,6 +35,10 @@ app.get('/about',(req,res)=>{
 
 app.get('/contact',(req,res)=>{
     res.send('Express Contact')
+})
+
+app.get('*',(req, res)=>{
+    res.render('404')
 })
 
 app.listen(2000,()=>{
